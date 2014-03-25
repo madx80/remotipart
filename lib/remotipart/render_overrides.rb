36de2554697501e18a0b53f279d979bf43e2b646
Madan Kommi <madan.kommi@icentris.com>
2014-03-25 15:25:35 -0600

    we should not escape body for content type json only.

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -13,7 +13,7 @@ module Remotipart
     def render_with_remotipart *args
       render_without_remotipart *args
       if remotipart_submitted?
-        textarea_body = response.content_type == 'text/html' ? html_escape(response.body) : response.body
+	textarea_body = (response.content_type == 'application/json') ? response.body : html_escape(response.body)
         response.body = %{<textarea data-type=\"#{response.content_type}\" data-status=\"#{response.response_code}\" data-statusText=\"#{response.message}\">#{textarea_body}</textarea>}
         response.content_type = Mime::HTML
       end
